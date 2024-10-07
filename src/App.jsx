bce0bd5c531fcd90e42e1828d1a8d82bad1d31e8
Mirwais <mirwais.sanaie@gmail.com>
2024-10-07 23:33:49 +0430

    add friend click and split click functinality added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -18,58 +20,65 @@ const initialFriends = [
     balance: 0,
   },
 ];
-
 function App() {
+  const [isSplit, setIsSplit] = useState(false);
+  const [isAddFriend, setAddFriend] = useState(false);
+
+  function handleIsSplit() {
+    setIsSplit((isSplit) => !isSplit);
+  }
+
+  function handleIsAddFriend() {
+    setAddFriend((isAddFriend) => !isAddFriend);
+  }
+
   return (
     <div className="container px-4 mx-auto space-y-5 md:space-y-0 grid md:grid-cols-2 grid-cols-1 my-16">
-      <FriendList />
-      <SplitFriend />
-      <AddFriend />
+      <FriendList
+        isAddFriend={isAddFriend}
+        handleIsAddFriend={handleIsAddFriend}
+      />
+      {isSplit && <SplitFriend onSplit={handleIsSplit} />}
+      {isAddFriend && <AddFriend />}
     </div>
   );
 }
 
 export default App;
 
-function FriendList() {
+function FriendList({ isAddFriend, handleIsAddFriend }) {
   return (
-    <div className="p-3 flex flex-col lg:w-2/3 mx-auto">
-      <Friend />
-      <Friend />
-      <Friend />
-      <div className="flex justify-center md:justify-end mt-6">
-        <button className="bg-orange-400 rounded-lg font-bold px-5 py-1 hover:opacity-70">
-          Add friend
-        </button>
+    <div className="p-3 flex flex-col lg:w-2/3 mx-auto space-y-3">
+      {initialFriends.map((el) => (
+        <Friend img={el.image} name={el.name} />
+      ))}
+      <div className="flex justify-center md:justify-end pt-6">
+        <Button onHandleClick={handleIsAddFriend}>
+          {isAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
     </div>
   );
 }
 
-function Friend() {
+function Friend({ onSplit, name, img }) {
   return (
-    <div className="flex justify-between p-3 items-center hover:bg-orange-100 hover:rounded-lg duration-100">
+    <div className="flex justify-between space-x-4 p-1 items-center hover:bg-orange-100 hover:rounded-lg duration-100">
       <div className="flex space-x-5">
-        <img
-          src="https://i.pravatar.cc/48?u=118836"
-          className="rounded-full"
-          alt=""
-        />
+        <img src={img} className="rounded-full" alt="" />
         <div>
-          <p className="font-bold">Clark</p>
+          <p className="font-bold">{name}</p>
           <p>You owe to clark</p>
         </div>
       </div>
       <div>
-        <button className="bg-orange-400 hover:bg-opacity-70 rounded-lg p-1 px-4 font-bold">
-          Select
-        </button>
+        <Button onHandleClick={onSplit}>Select</Button>
       </div>
     </div>
   );
 }
 
-function SplitFriend() {
+function SplitFriend({ onSplit }) {
   return (
     <div className="bg-orange-100 md:w-2/3 p-5 rounded-lg">
       <h1 className="text-2xl font-bold mb-5">Split a Bill with Sarah</h1>
@@ -106,10 +115,8 @@ function SplitFriend() {
           <input type="number" className="w-20 h-7 text-center border" />
         </div>
 
-        <div className="flex justify-end">
-          <button className="bg-orange-500 rounded-lg font-bold mt-4 px-5 py-1 hover:opacity-70">
-            Split bill
-          </button>
+        <div className="flex justify-end pt-3">
+          <Button onHandleClick={onSplit}>Split Bill</Button>
         </div>
       </div>
     </div>
@@ -134,10 +141,19 @@ function AddFriend() {
         <input type="text" className="h-7 text-center" />
       </div>
       <div className="flex justify-end">
-        <button className="bg-orange-400 font-bold py-1 px-7 rounded-lg">
-          Add
-        </button>
+        <Button>Add</Button>
       </div>
     </div>
+  );
+}
+
+function Button({ children, onHandleClick }) {
+  return (
+    <button
+      onClick={onHandleClick}
+      className="bg-orange-400 hover:bg-opacity-70 rounded-lg p-1 px-6 font-bold"
+    >
+      {children}
+    </button>
   );
 }
